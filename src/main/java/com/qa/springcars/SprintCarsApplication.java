e8bfee0ef65edd987156767aa31fdc87e88bc01c
DavrosB <dave_bell@blueyonder.co.uk>
2021-07-05 11:03:48 +0100

    DB added intial files

---
+++ src/main/java/com/qa/springcars/SprintCarsApplication.java
@@ -0,0 +1,13 @@
+package com.qa.springcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/springcars/SprintCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.springcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
